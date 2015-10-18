01f8cce7969ec8ad373cbbcb26cf3603c36259ce
yangshebing <shebing_yang@163.com>
2015-10-18 19:24:26 +0800

    first create the fast guess project

---
+++ FastGuess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastGuess
+//
+//  Created by 杨社兵 on 15/10/18.
+//  Copyright © 2015年 FAL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
