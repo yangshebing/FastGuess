dc8d076e0f1f4352c638c1ec80c595d9a2582390
yangshebing <shebing_yang@163.com>
2015-10-23 00:12:24 +0800

    swift单例的四种写法

--- FastGuess/UserInfoManager.swift
+++ FastGuess/UserInfoManager.swift
@@ -7,13 +7,68 @@
 //
 
 import Foundation
-public class UserInfoManager {
-    var userName:String?
-    var avatar:String?
-    var userId = 0
-    var age:String?
-    var sex:String?
-    public static let sharedInstance: UserInfoManager = {
-        return UserInfoManager()
-        }()
-}
+//写法一：
+//public class UserInfoManager {
+//    var userName:String?
+//    var avatar:String?
+//    var userId = 0
+//    var age:String?
+//    var sex:String?
+//    public static let sharedInstance: UserInfoManager = {
+//        return UserInfoManager()
+//        }()
+//}
+
+//写法二：
+//let _sharedInstance =  ()
+//class UserInfoManager {
+//    var userName:String?
+//    var avatar:String?
+//    var userId = 0
+//    var age:String?
+//    var sex:String?
+//    class var sharedInstance : UserInfoManager{
+//        return _sharedInstance
+//    }
+//}
+
+//写法三：
+//class UserInfoManager {
+//    var userName:String?
+//    var avatar:String?
+//    var userId = 0
+//    var age:String?
+//    var sex:String?
+//    class var sharedInstance:  UserInfoManager {
+//        struct Static {
+//            static let instance: UserInfoManager = UserInfoManager();
+//        }
+//        return Static.instance
+//    }
+//}
+
+//写法四：
+//class UserInfoManager {
+//    var userName:String?
+//    var avatar:String?
+//    var userId = 0
+//    var age:String?
+//    var sex:String?
+//
+//    class var sharedInstance :  UserInfoManager {
+//        struct Static {
+//            static var onceToken:dispatch_once_t = 0;
+//            static var instance: UserInfoManager? = nil;
+//        }
+//        
+//        dispatch_once(&Static.onceToken) {
+//            Static.instance = UserInfoManager()
+//        }
+//        
+//        //这个地方有什么区别呢？
+////        dispatch_once(&Static.onceToken) { () -> Void in
+////             Static.instance = UserInfoManager()
+////        }
+//        return Static.instance!
+//    }
+//}
